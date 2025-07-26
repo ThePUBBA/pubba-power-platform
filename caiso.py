be4a06c690ab3d5a9dfa6326486df90f3d1241d6
Christopher Albanese <christopheralbanese@ChristophersMBP.hsd1.co.comcast.net>
2025-07-26 08:06:59 -0600

    Debug: print CAISO zip contents

--- caiso.py
+++ caiso.py
@@ -1,34 +1,6 @@
-import pandas as pd
-import requests
-from datetime import datetime
-from io import StringIO
-import zipfile
-import io as io_module
-
-def fetch_lmp_data(location="TH_NP15_GEN-APND", market="LMP", date=None):
-    if date is None:
-        date = datetime.utcnow().strftime("%Y%m%d")
-
-    url = "http://oasis.caiso.com/oasisapi/SingleZip"
-    params = {
-        "queryname": "PRC_LMP",
-        "startdatetime": f"{date}T00:00-0000",
-        "enddatetime": f"{date}T23:59-0000",
-        "market_run_id": market,
-        "node": location,
-        "version": 1,
-        "resultformat": 6
-    }
-
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-
-    with zipfile.ZipFile(io_module.BytesIO(response.content)) as zf:
-        csv_file = [f for f in zf.namelist() if f.endswith('.csv')][0]
-        with zf.open(csv_file) as f:
-            df = pd.read_csv(f)
-
-    df = df[["LOCATION", "OPR_DT", "INTERVAL_NUM", "LMP"]]
-    df.columns = ["location", "date", "hour", "price"]
-    return df
+with zipfile.ZipFile(io_module.BytesIO(response.content)) as zf:
+    print("ZIP contents:", zf.namelist())  # 👈 Add this debug line
+    csv_file = [f for f in zf.namelist() if f.endswith('.csv')][0]
+    with zf.open(csv_file) as f:
+        df = pd.read_csv(f)
 
