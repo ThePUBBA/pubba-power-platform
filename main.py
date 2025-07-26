5e68ba3c5c0abc5c692986367e7a8d1d5a2508a5
Christopher Albanese <christopheralbanese@ChristophersMBP.hsd1.co.comcast.net>
2025-07-26 06:03:40 -0600

    Initial commit – Only1 LMP API

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from gridstatus import CAISO
+from datetime import date
+
+app = FastAPI()
+iso = CAISO()
+
+@app.get("/")
+def root():
+    return {"message": "Only1 LMP API is running"}
+
+@app.get("/lmp")
+def get_lmp(market: str = "RTM", location: str = "TH_NP15_GEN-APND"):
+    today = date.today().strftime("%Y-%m-%d")
+    data = iso.get_lmp(date=today, market=market, locations=[location])
+    return data.to_dict(orient="records")
